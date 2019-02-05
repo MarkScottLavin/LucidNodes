82a413340aae4cf68547701095c117e7ba19c878
Mark Scott Lavin <markscottlavin@gmail.com>
2019-02-04 22:25:16 -0800

    Updating Microservices Model Test for GAE

--- microservices/routes.js
+++ microservices/routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-//const app = express();
+const app = express();
 const router = express.Router();
 const qs = require('querystring');
 const bodyParser = require("body-parser");
