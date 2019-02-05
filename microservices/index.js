8f7c23f5367174365dddf9a8e81a1833a0cfe618
Mark Scott Lavin <markscottlavin@gmail.com>
2019-02-04 23:12:15 -0800

    Update Microservices Model Test for GAE

--- microservices/index.js
+++ microservices/index.js
@@ -2,10 +2,10 @@ const server = require('./server');
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
-const serveStatic = require('serve-static');
-const serveIndex = require('serve-index');
-const staticPaths = require('./defineStaticPaths');
-const htmlMethods = require('./js/htmlMethods');  // import custom methods for handling HTML
+//const serveStatic = require('serve-static');
+//const serveIndex = require('serve-index');
+//const staticPaths = require('./defineStaticPaths');
+//const htmlMethods = require('./js/htmlMethods');  // import custom methods for handling HTML
 const app = express();
 const routes = require('./routes');
 
